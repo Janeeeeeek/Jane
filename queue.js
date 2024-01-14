6f814bc8d4c7119738e3bedd43a9976c77b73308
Janeeeeeek <156202665+Janeeeeeek@users.noreply.github.com>
2024-01-14 14:49:43 +0700

    Create queue.js

---
+++ queue.js
@@ -0,0 +1,24 @@
+function queue(customerQueue, couter) {
+    console.log("คิวที่ " + customerQueue + " เชิญที่ช่องบริการหมายเลข " + couter + " ค่ะ")
+} 
+
+queue(230, 3)
+
+// ลูกค้า 1-4 คน กด A
+// ลูกค้า 5 คนขึ้นไป กด B
+
+function count(person, type) {
+    console.log("ลูกค้าจำนวน " + person + " คน" + " กด " + type)
+}   
+
+count(1, "A")
+count(2, "A")
+count(3, "A")
+count(4, "A")
+count(5, "B")
+
+
+
+
+
+
