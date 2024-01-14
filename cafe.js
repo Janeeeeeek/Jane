2fbb99ce81cb993decf1fd725feac3ea724250ea
Janeeeeeek <156202665+Janeeeeeek@users.noreply.github.com>
2024-01-14 14:58:56 +0700

    Create cafe.js

---
+++ cafe.js
@@ -0,0 +1,10 @@
+let shopName = " code class coffee";
+
+function sayWelcome(customerName){
+    console.log("Welcome K." + customerName + " to" + shopName)
+}
+
+
+sayWelcome("Jane")
+sayWelcome("Pluto")
+sayWelcome("Bew")
